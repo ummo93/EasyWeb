997b50087e863f47e528e1a638df011705350d6d
austere93 <ummo93@gmail.com>
2017-01-22 22:53:43 +0200

    add md5 method to App class

--- src/main/java/framework/App.java
+++ src/main/java/framework/App.java
@@ -200,7 +200,18 @@ public class App {
             return "null";
         }
     }
-
+    public static String MD5(String md5) {
+        try {
+            java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+            byte[] array = md.digest(md5.getBytes());
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < array.length; ++i) {
+            sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1,3));
+        }
+            return sb.toString();
+        } catch (java.security.NoSuchAlgorithmException e) {}
+        return null;
+    }
     private static String bytesToHexStr(byte[] raw) {
         /** Преобразование байтового массива в hex-строку */
         char[] kDigits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
